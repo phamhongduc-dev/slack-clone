6d84397a925247ab8dc2820d3da11b21ca0539e3
phamhongduc <nroduc@gmail.com>
2024-10-03 01:04:25 +0700

    channel header

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -45,7 +45,7 @@ export default function WorkspaceIdPage() {
     workspaceId
   ])
 
-  if (workspaceLoading || channelsLoading) {
+  if (workspaceLoading || channelsLoading || memberLoading) {
     return (
       <div className='h-full flex-1 items-center justify-center flex-col gap-2'>
         <Loader className='size-6 animate-spin text-muted-foreground' />
@@ -53,7 +53,7 @@ export default function WorkspaceIdPage() {
     )
   }
 
-  if (!workspace) {
+  if (!workspace || !member) {
     return (
       <div className='h-full flex-1 flex items-center justify-center flex-col gap-2'>
         <TriangleAlert className='size-6 text-muted-foreground' />
