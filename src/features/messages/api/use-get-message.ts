c744b2559575a6adcbc90527e3c4f40ad0b69a3e
phamhongduc <nroduc@gmail.com>
2024-10-06 00:39:23 +0700

    infinite load feature

--- src/features/messages/api/use-get-message.ts
+++ src/features/messages/api/use-get-message.ts
@@ -3,7 +3,7 @@ import { usePaginatedQuery } from 'convex/react'
 import { api } from '../../../../convex/_generated/api'
 import { Id } from '../../../../convex/_generated/dataModel'
 
-const BATCH_SIZE = 20
+const BATCH_SIZE = 3
 
 interface UseGetMessagesProps {
   channelId?: Id<'channels'>
