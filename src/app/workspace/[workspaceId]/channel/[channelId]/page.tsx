68f6d072645101853397891c3bd4a121fe83a31d
phamhongduc <nroduc@gmail.com>
2024-10-06 03:55:38 +0700

    messages get api

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -3,7 +3,7 @@
 import { Loader, TriangleAlert } from 'lucide-react'
 
 import { useGetChannel } from '@/features/channels/api/use-get-channel'
-import { useGetMessages } from '@/features/messages/api/use-get-message'
+import { useGetMessages } from '@/features/messages/api/use-get-messages'
 
 import { useChannelId } from '@/hooks/use-channel-id'
 import { MessageList } from '@/components/message-list'
